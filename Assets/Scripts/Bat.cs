e005f8bed307c90b754c1c12c9796517467d37b0
Mac 7 <Mac 7>
2017-12-08 16:41:42 +0530

    Ignored stuff

--- Assets/Scripts/Bat.cs
+++ Assets/Scripts/Bat.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bat : MonoBehaviour {
-
+	//handle bat stuff
 	public bool hasBallbeenHit;
 	public Vector3 hitForce;
 	public float factor;
